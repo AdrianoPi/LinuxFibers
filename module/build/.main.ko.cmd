2ebf4d2a7d00b052fc6092733b994d2b94bf1153
aosv <gabriot1994@gmail.com>
2019-03-01 10:55:37 +0100

    cleanup

--- module/build/.main.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/adriano/AOSV/manualmerge/LinuxFibers/module/build/main.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds  --build-id  -o /home/adriano/AOSV/manualmerge/LinuxFibers/module/build/main.ko /home/adriano/AOSV/manualmerge/LinuxFibers/module/build/main.o /home/adriano/AOSV/manualmerge/LinuxFibers/module/build/main.mod.o ;  true
